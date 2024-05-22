9db17859d79b012fb8c0d9c9a5509db366bfc2fc
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-05-22 19:31:44 +0300

    Sum of Numbers

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -1,16 +1,27 @@
+//Write a program to print the sum of the two numbers
 #include <iostream>
 
 using namespace std;
 
 int main() {
-	int var1 = 10;
-	const int var2 = 20;
+	/*
+	There are two approaches to writing variable and below are two of them:
+	int var1 = 10, var2 = 20;
+	*/
+	int a = 0;
+	int b = 0;
+	int sum = 0;
 
-	var1 = 100;
-	//var2 = 200;
+	cout << "Enter the first number: " << endl;
+	cin >> a;
 
-	cout << var1 << endl;
-	cout << var2 << endl;
+	cout << "Enter the second number: " << endl;
+	cin >> b;
+
+
+	sum = a + b;
+	cout << "The sum of the two numbers is: " << sum;
+
 
 	return 0;
 }
