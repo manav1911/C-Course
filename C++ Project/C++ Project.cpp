644a40d42fe33e5c77e7eec5e6de13c29ca90d9b
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-05-23 06:26:29 +0300

    Finding the area of a square

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -1,27 +1,19 @@
-//Write a program to print the sum of the two numbers
+//Write a program to print the area of a square
 #include <iostream>
 
 using namespace std;
 
 int main() {
-	/*
-	There are two approaches to writing variable and below are two of them:
-	int var1 = 10, var2 = 20;
-	*/
-	int a = 0;
-	int b = 0;
-	int sum = 0;
+	int sides = 0;
+	int area = 0;
 
-	cout << "Enter the first number: " << endl;
-	cin >> a;
+	//User Input
+	cout << "Enter the lenght of the square: ";
+	cin >> sides;
 
-	cout << "Enter the second number: " << endl;
-	cin >> b;
-
-
-	sum = a + b;
-	cout << "The sum of the two numbers is: " << sum;
-
+	//Calculation
+	area = sides * sides;
+	cout << "The area of the square is: " << area;
 
 	return 0;
 }
