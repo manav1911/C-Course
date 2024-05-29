098fb9ff4e8c50505ccc525a9e73c3e0d167403d
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-05-29 05:27:49 +0300

    Odd Even Checker

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -1,27 +1,20 @@
-//Write a program to print the sum of the two numbers
+//Write a program to check a number is odd or even
 #include <iostream>
 
 using namespace std;
 
 int main() {
-	/*
-	There are two approaches to writing variable and below are two of them:
-	int var1 = 10, var2 = 20;
-	*/
 	int a = 0;
-	int b = 0;
-	int sum = 0;
 
-	cout << "Enter the first number: " << endl;
+	cout << "Enter your number: ";
 	cin >> a;
 
-	cout << "Enter the second number: " << endl;
-	cin >> b;
-
-
-	sum = a + b;
-	cout << "The sum of the two numbers is: " << sum;
-
+	if (a % 2 == 0) {
+		cout << "The number is even!";
+	}
+	else {
+		cout << "The number is odd!";
+	}
 
 	return 0;
 }
