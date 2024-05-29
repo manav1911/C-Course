84d113a7cece9b633af1a58f561e9cfec62dc213
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-05-29 05:12:58 +0300

    If-Condition

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -1,27 +1,15 @@
-//Write a program to print the sum of the two numbers
 #include <iostream>
-
 using namespace std;
 
 int main() {
-	/*
-	There are two approaches to writing variable and below are two of them:
-	int var1 = 10, var2 = 20;
-	*/
-	int a = 0;
-	int b = 0;
-	int sum = 0;
+	int marks = 0;
 
-	cout << "Enter the first number: " << endl;
-	cin >> a;
+	cout << "Enter your marks: ";
+	cin >> marks;
 
-	cout << "Enter the second number: " << endl;
-	cin >> b;
-
-
-	sum = a + b;
-	cout << "The sum of the two numbers is: " << sum;
-
+	if (marks >= 60) {
+		cout << "You have passed!";
+	}
 
 	return 0;
 }
