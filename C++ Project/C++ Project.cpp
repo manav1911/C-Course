d7d0e0534ca0c3cf379b6f22b522261cd97e2e38
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-05-30 05:32:52 +0300

    Update C++ Project.cpp

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -1,19 +1,9 @@
-//Write a program to swap two numbers
+//Master
 #include <iostream>
 using namespace std;
 
 int main() {
-	int a = 10;
-	int b = 20;
-	int temp = 0;
-
-	temp = a;
-
-	a = b;
-	b = temp;
-
-	cout << "a = " << a << endl;
-	cout << "b = " << b << endl;
-
+	
+	
 	return 0;
 }
