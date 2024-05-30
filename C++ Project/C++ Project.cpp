0ff3cfb7ce4da8ebf393e8e413e1383400025567
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-05-30 06:10:38 +0300

    Upper/Lower Case

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -1,10 +1,23 @@
-//Master
+//Check if the letter is upper or lower case
 #include <iostream>
 
 using namespace std;
 
 int main() {
+	char letter;
 
+	cout << "Enter any character: ";
+	cin >> letter;
+
+	if (letter>='A' && letter <= 'Z'){
+		cout << "The letter " << letter << " is upper case";
+	}
+	else if (letter >= 'a' && letter <= 'z') {
+		cout << "The letter " << letter << " is lower case";
+	}
+	else {
+		cout << "Invalid Input";
+	}
 
 	return 0;
 }
