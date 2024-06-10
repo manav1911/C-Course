432c2c8616d5b71ce12ad0dd725af5dc215a4b64
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-06-10 06:11:56 +0300

    While Loop

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -1,10 +1,18 @@
-//Master
 #include <iostream>
 
 using namespace std;
 
 int main() {
+	int value = 0;
+	int x = 1;
 
+	cout << "Enter the number: ";
+	cin >> value;
+
+	while (x <= 10) {
+		cout << endl << value << " * " << x << " = " << value * x;
+		x++;
+	}
 
 	return 0;
 }
