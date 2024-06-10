6a926a99b06ec0083bf4c39726c796a287531322
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-06-10 05:42:44 +0300

    star

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -1,10 +1,16 @@
-//Master
 #include <iostream>
 
 using namespace std;
 
 int main() {
+	int x = 0, y = 0;
 
+	for (x = 0; x < 5; x++) {
+		for (y = 0; y < x + 1; y++) {
+			cout << "*";
+		}
+		cout << endl;
+	}
 
 	return 0;
 }
