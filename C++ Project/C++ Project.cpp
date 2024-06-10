566031c7ada82ef71cc1b5d548ee427a1b5e174c
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-06-10 05:22:19 +0300

    FOR Loop

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -1,10 +1,18 @@
-//Master
+//Program to print the table of input No.
 #include <iostream>
 
 using namespace std;
 
 int main() {
+	int value = 0;
 
+	cout << "Enter a number: ";
+	cin >> value;
+
+	//For Loop
+	for (int i = 1; i <= 10; i++) {
+		cout << endl << value << " * " << i << " = " << value * i;
+	}
 
 	return 0;
 }
