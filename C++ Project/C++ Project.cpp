07960f842b4dcd13c9c7f208f9254c628780f23d
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-06-10 06:03:18 +0300

    sum of first 10 natural numbers

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -1,10 +1,16 @@
-//Master
+//Program in C++ to find the sum of first 10 natural numbers.
 #include <iostream>
 
 using namespace std;
 
 int main() {
+	int x, sum = 0;
 
+	for (x = 1; x <= 10; x++) {
+		sum = sum + x;
+	}
+
+	cout << "The sum of the first 10 natural numbers: " << sum << endl;
 
 	return 0;
 }
