faa1d57c0325b8c8a2772ef35fffc533e77d8fc8
Manavkumar Patel <manavpate0@gmail.com>
2024-06-11 16:19:17 +0300

    Factorial of a number - For Loop

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -1,9 +1,17 @@
-//Master
 #include <iostream>
 using namespace std;
 
 int main() {
-	
-	
+	int x = 0, factorial = 1;
+
+	cout << "Enter the starting number for the sum of the first 10 numbers: ";
+	cin >> x;
+
+	for (int i = x; i > 0; i--) {
+		factorial = factorial * i;
+	}
+
+	cout << "The factorial of " << x << " you entered is: " << factorial << endl;	
+
 	return 0;
 }
