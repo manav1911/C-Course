2f05690fc6c19a718cf6ee55de92fe707a29349c
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-06-11 20:04:45 +0300

    Finding even numbers in an array

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -1,10 +1,19 @@
-//Master
 #include <iostream>
 
 using namespace std;
 
 int main() {
+	int arr[] = { 12,8,15,6,2,9,1 };
+	int arraysize = 0;
 
+	arraysize = sizeof(arr) / sizeof(arr[0]);
+	cout << "Length of array is: " << arraysize;
+
+	for (int i = 0; i < arraysize; i++) {
+		if (arr[i] % 2 == 0) {
+			cout << endl << arr[i];
+		}
+	}
 
 	return 0;
 }
