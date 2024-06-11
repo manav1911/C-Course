76e34cb686ffb48796a9d4e9a7150c71a4b1357d
Manavkumar Patel <manavpate0@gmail.com>
2024-06-11 16:00:18 +0300

    Assignment - Do While Loop

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -1,9 +1,16 @@
-//Master
 #include <iostream>
+
 using namespace std;
 
 int main() {
+	int value = 1, sum = 0;
 	
-	
+	do {
+		sum = sum + value;
+		value++;
+	} while (value <= 10);
+
+	cout << "The sum of the first 10 natural numbers is: " << sum << endl;
+
 	return 0;
 }
