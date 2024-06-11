3a3e258ce4c105f5d42b58dcd01d2b0799daaca9
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-06-11 20:18:20 +0300

    Sum and multiplication of values in an array

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -4,7 +4,19 @@
 using namespace std;
 
 int main() {
+	int arr[] = { 12,8,15,6,2,9,1 };
+	int arraysize = 0, sum = 0, multi = 1;
 
+	arraysize = sizeof(arr) / sizeof(arr[0]);
+	cout << "Length of array is: " << arraysize << endl;
+
+	for (int i = 0; i < arraysize; i++) {
+		sum = sum + arr[i];
+		multi = multi * arr[i];
+	}
+
+	cout << "The sum of the array is: " << sum << endl;
+	cout << "The multiplication of the array is: " << multi << endl;
 
 	return 0;
 }
