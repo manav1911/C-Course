d895c3a9115e0c9af2086a0777ef8029ccf896a8
Manavkumar Patel <manavpate0@gmail.com>
2024-06-11 16:09:53 +0300

    For Loop

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -1,9 +1,19 @@
-//Master
+//Program in C++ to find the sum of next 10 numbers
 #include <iostream>
 using namespace std;
 
 int main() {
-	
+	int x = 0, sum = 0;
+
+	cout << "Enter the starting number for the sum of the first 10 numbers: ";
+	cin >> x;
+
+	for (int i = 1; i <= 10; i++) {
+		x++;
+		sum = sum + x;
+	}
+
+	cout << endl <<"The sum of the 10 numbers after you have entered is: " << sum << endl;
 	
 	return 0;
 }
