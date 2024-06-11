0032a0cb92022ab0d7e99cb198437426ac5f5972
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-06-11 19:28:27 +0300

    Updated

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -1,10 +1,15 @@
-//Master
 #include <iostream>
 
 using namespace std;
 
 int main() {
+	int x = 1, sum = 0;
 
+	while (x <= 10) {
+		sum = sum + x;
+		x++;
+	}
+	cout << "The sum of the first 10 natural numbers: " << sum;
 
 	return 0;
 }
