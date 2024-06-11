07c956ebd141f41282d4a4d0a0476a46c8997a8f
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-06-11 19:48:30 +0300

    Finding minimum number in array value from user inputs

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -1,10 +1,25 @@
-//Master
 #include <iostream>
 
 using namespace std;
 
 int main() {
+	//Creating an array
+	int arr[5] = {  };
 
+	for (int i = 0; i < 5; i++) {
+		cout << endl << "Enter no. " << i << " : ";
+		cin >> arr[i];
+	}
+
+	int min = arr[0];
+
+	for (int i = 1; i < 5; i++) {
+		if (min > arr[i]) {
+			min = arr[i];
+		}
+	}
+
+	cout << endl << "The minimum value is: " << min;
 
 	return 0;
 }
