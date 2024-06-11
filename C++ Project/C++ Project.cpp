fb112f88ece58ff458159d427f3a4b33dfa4debd
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-06-11 19:34:44 +0300

    Find the length of an array

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -1,10 +1,13 @@
-//Master
 #include <iostream>
 
 using namespace std;
 
 int main() {
+	int arr[] = { 100,200,300,400,500 };
+	int arraysize = 0;
 
+	arraysize = sizeof(arr) / sizeof(arr[0]);
+	cout << "Lenght of array is: " << arraysize;
 
 	return 0;
 }
