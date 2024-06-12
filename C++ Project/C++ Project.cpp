a8d3725c4ed9116e9213923ebc3aae970c8134ee
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-06-12 05:57:58 +0300

    Assignment to find the sum of numbers in an array for user inputted values

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -1,10 +1,23 @@
-//Master
 #include <iostream>
 
 using namespace std;
 
 int main() {
+	int arr[5] = {};
+	int arraysize = 0;
+	int sum = 0;
 
+	for (int i = 0; i < 5; i++) {
+		cout << "Enter the numbers: ";
+		cin >> arr[i];
+	}
+
+	arraysize = sizeof(arr) / sizeof(arr[0]);
+
+	for (int i = 0; i < arraysize; i++) {
+		sum = sum + arr[i];
+	}
+	cout << "The sum of the numbers entered is: " << sum << endl;
 
 	return 0;
 }
