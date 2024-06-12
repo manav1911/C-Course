7687bd150a35b60b5fe2672f93f9646e1cf675fd
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-06-12 05:31:49 +0300

    User input and output of data in 2D array

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -4,7 +4,23 @@
 using namespace std;
 
 int main() {
+	int arr[5][5];
+	
+	//Input
+	for (int i = 0; i < 5; i++) { //Representing the row
+		for (int j = 0; j < 5; j++) { //Representing the column
+			cout << "Row is: " << i << " and column is: " << j << " Enter a value: ";
+			cin >> arr[i][j];
+		}
+	}
 
+	//Print
+	for (int i = 0; i < 5; i++) { //Representing the row
+		for (int j = 0; j < 5; j++) { //Representing the column
+			cout << endl << "Row is: " << i << " and column is: " << j << " The value is: ";
+			cout << arr[i][j];
+		}
+	}
 
 	return 0;
 }
