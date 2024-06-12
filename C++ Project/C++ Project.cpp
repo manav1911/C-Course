702723d67e4fa3f0117eeae56e742a2165652d0c
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-06-12 05:44:21 +0300

    Sum of values in a 2D Array

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -4,7 +4,25 @@
 using namespace std;
 
 int main() {
+	int arr[5][5];
+	int sum = 0;
 
+	//Input
+	for (int i = 0; i < 5; i++) { //Representing the row
+		for (int j = 0; j < 5; j++) { //Representing the column
+			cout << "Row is: " << i << " and column is: " << j << " Enter a value: ";
+			cin >> arr[i][j];
+		}
+	}
+
+	//Sum Logic
+	for (int i = 0; i < 5; i++) { //Representing the row
+		for (int j = 0; j < 5; j++) { //Representing the column
+			sum = sum + arr[i][j];
+
+		}
+	}
+	cout << "Sum is: " << sum;
 
 	return 0;
 }
