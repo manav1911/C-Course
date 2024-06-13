d169f9b8f2a4e995feef6a5eb10038c22145b28e
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-06-13 05:49:25 +0300

    Strings example

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -1,10 +1,23 @@
-//Master
 #include <iostream>
-
+#include<string>
 using namespace std;
 
 int main() {
+	string firstname, lastname, address;
 
+	cout << "Enter your First Name: ";
+	cin >> firstname;
+
+	cout << "Enter your Last Name: ";
+	cin >> lastname;
+
+	cout << "Enter your Address: ";
+	getline(cin.ignore(), address);   /* We have used getline() to also accept spaces in user inputs,
+	and we have used the .ignore() because we need to flush the newline out of the stream*/
+
+	string fullname = firstname.append(lastname);
+
+	cout << "Your Fullnames are: " << fullname << " and address is: " << address << endl;
 
 	return 0;
 }
