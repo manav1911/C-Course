b43d3ce63146faef5ba5f5c7da88e4a6bf0bdd08
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-06-13 05:59:11 +0300

    Compare strings

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -1,10 +1,23 @@
-//Master
-#include <iostream>
-
+//Master//
+#include<iostream>
+#include<string>
 using namespace std;
 
 int main() {
+	string var1 = "", var2 = "";
 
+	cout << "Enter a string: ";
+	getline(cin, var1);
+
+	cout << "Enter another string: ";
+	getline(cin, var2);
+
+	if (var1.compare(var2) == 0) {		//We have used the .compare() function to compare the strings
+		cout << "Both the strings are the same." << endl;
+	}
+	else {
+		cout << "Both the strings are not the same." << endl;
+	}
 
 	return 0;
 }
