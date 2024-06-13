37c75f554ae597c35478296f0ebd7ff410b7fc06
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-06-13 06:21:36 +0300

    Reverse String

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -4,6 +4,16 @@
 using namespace std;
 
 int main() {
+	string var1 = "";
+
+	cout << "Enter a string: ";
+	getline(cin, var1);
+
+	cout << endl;
+	//Logic
+	for (int i = var1.size(); i >= 0; i--) {
+		cout << var1[i];
+	}
 
 
 	return 0;
