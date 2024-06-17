59a335fe3bd24af107d0a1e2f2337360901f50a0
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-06-17 14:42:06 +0300

    Removing all the vowels

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -4,7 +4,17 @@
 using namespace std;
 
 int main() {
+	string var = "";
 
+	cout << "Enter a string: ";
+	getline(cin, var);
 
+	for (int i = 0; i <= var.size(); i++) {
+		if (var[i] == 'a' || var[i] == 'e' || var[i] == 'i' || var[i] == 'o' || var[i] == 'u' || var[i] == 'A' || var[i] == 'E' || var[i] == 'I' || var[i] == 'O' || var[i] == 'U') {
+			var[i] = ' ';
+		}
+	}
+
+	cout << var;
 	return 0;
 }
