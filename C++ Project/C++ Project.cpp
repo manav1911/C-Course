48540676b6456897b4e1fce0776d226d72d6fd98
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-06-17 14:51:56 +0300

    replaving the first character with & sign

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -4,7 +4,20 @@
 using namespace std;
 
 int main() {
+	string var = "";
+	char temp;
 
+	cout << "Enter a string: ";
+	cin >> var;
 
+	temp =var[0];
+
+	for (int i = 1; i <= var.size(); i++) {
+		if (var[i] == temp) {
+			var[i] = '&';
+		}
+	}
+
+	cout << var;
 	return 0;
 }
