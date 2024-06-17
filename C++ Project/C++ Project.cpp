f615ffecfbbe730a12cbc765d9be69cae7d9ecca
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-06-17 15:02:17 +0300

    Replacing words with user inputs

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -4,7 +4,22 @@
 using namespace std;
 
 int main() {
+	string var = "", var1 = "", var2 = "";
 
+	cout << "Enter a string: ";
+	getline(cin, var);
+
+	cout << "Enter the string to find: ";
+	getline(cin, var1);
+
+	cout << "Enter the string to replace: ";
+	getline(cin, var2);
+
+	while (var.find(var1) != string::npos) {
+		var.replace(var.find(var1), var1.size(), var2);
+	}
+
+	cout << var;
 
 	return 0;
 }
