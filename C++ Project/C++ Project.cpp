8bfc8afc4053958cc7279f060667f06bcd3b8e7c
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-06-19 06:14:10 +0300

    Function types

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -3,8 +3,75 @@
 #include<string>
 using namespace std;
 
-int main() {
+/*
+//takes nothing and returns nothing
+void fun(void);
 
+int main() {
+	fun();
 
 	return 0;
+}
+
+void fun(void) {
+	cout << "Hello";
+}
+*/
+
+/*
+//takes something and returns nothing
+void fun(int);
+
+int main() {
+	int val = 0;
+
+	cout << "Enter a value: ";
+	cin >> val;
+
+	fun(val);
+
+	return 0;
+}
+
+void fun(int a) {
+	cout << endl << a;
+}
+*/
+
+/*
+//takes nothing and returns something
+int fun(void);
+
+int main() {
+	int val = 0;
+
+	val = fun();
+	cout << "Value is: " << val;
+	return 0;
+}
+
+int fun(void) {
+	return 10;
+}
+*/
+
+//takes something and returns something
+int fun(int);
+
+int main() {
+	int val = 0;
+
+	cout << "Enter a value: ";
+	cin >> val;
+
+	val = fun(val);
+
+	cout << endl << "Value is: " << val;
+
+	return 0;
+}
+
+int fun(int x) {
+	x = x * x;
+	return x;
 }
