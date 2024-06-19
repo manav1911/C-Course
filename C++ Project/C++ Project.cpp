e6a6966958880561cf85531f9728c0f91c16120e
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-06-19 05:40:17 +0300

    Comparing Length

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -4,7 +4,20 @@
 using namespace std;
 
 int main() {
+	string var1 = "", var2 = "";
 
+	cout << "Enter the first string: ";
+	getline(cin, var1);
 
+	cout << "Enter the first string: ";
+	getline(cin, var2);
+
+	if (var2.size() == var1.size()) {
+		cout << "The string length is equal.";
+	}
+	else {
+		cout << "The string length is not equal.";
+	}
+	
 	return 0;
 }
