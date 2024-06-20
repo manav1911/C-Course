c392e02ac6bde103563d31ca446397adc5f1f333
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-06-20 05:49:11 +0300

    Write a C++ Program to find number is even or odd using function.

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -3,8 +3,24 @@
 #include<string>
 using namespace std;
 
-int main() {
+void check(int);
 
+int main() {
+	int num = 0;
+
+	cout << "Enter an integer: ";
+	cin >> num;
+	
+	check(num);
 
 	return 0;
+}
+
+void check (int x) {
+	if (x % 2 == 0) {
+		cout << "The value is even.";
+	}
+	else {
+		cout << "The value is odd";
+	}
 }
