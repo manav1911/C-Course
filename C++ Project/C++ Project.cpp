9274ee51d921c7da396e0400daafdd699b0082e2
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-06-20 05:39:49 +0300

    inline function

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -3,8 +3,15 @@
 #include<string>
 using namespace std;
 
+void multiply(int, int);
+
 int main() {
 
+	multiply(10000, 20000);
 
 	return 0;
+}
+
+inline void multiply(int x, int y) {
+	cout << "The multiplication of the two integer digits is: " << x * y << endl;
 }
