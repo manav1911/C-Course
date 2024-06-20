e2339e1ba469116a75b881b46a61e20748927420
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-06-20 05:07:48 +0300

    Function Overloading Integers

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -3,8 +3,20 @@
 #include<string>
 using namespace std;
 
-int main() {
+void add(int, int);
+void add(float, float);
 
+int main() {
+	add(10, 20);
+	add(10.7f, 10.3f);
 
 	return 0;
+}
+
+void add(int x, int y) {
+	cout << "The sum of the two integer digits is: " << x + y << endl;
+}
+
+void add(float x, float y) {
+	cout << "The sum of the two float digits is: " << x + y;
 }
