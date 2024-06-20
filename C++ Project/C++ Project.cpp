63fb9cf6ba81b14438ad8f64c5f5faa085641c68
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-06-20 05:16:11 +0300

    Adding string values in the function overloading

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -5,10 +5,12 @@ using namespace std;
 
 void add(int, int);
 void add(float, float);
+void add(string, string);
 
 int main() {
 	add(10, 20);
 	add(10.7f, 10.3f);
+	add("Manav", "Patel");
 
 	return 0;
 }
@@ -18,5 +20,9 @@ void add(int x, int y) {
 }
 
 void add(float x, float y) {
-	cout << "The sum of the two float digits is: " << x + y;
+	cout << "The sum of the two float digits is: " << x + y << endl;
+}
+
+void add(string x, string y) {
+	cout << "The string you added is: " << x + " " + y;
 }
