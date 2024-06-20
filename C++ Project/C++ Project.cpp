1d95b1fdce7d115d1a2affed49dae003da0f46c2
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-06-20 06:07:14 +0300

    Armstrong number check

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -1,10 +1,38 @@
-//
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
 using namespace std;
 
-int main() {
+void fun(int number);
 
+void main() {
+	int num, originalNum, remainder, result = 0;
 
-	return 0;
+	cout << "Enter a three-digit integer: ";
+	cin >> num;
+
+	fun(num);
+}
+
+void fun(int number) {
+
+	int originalNum, remainder, result = 0;
+
+	originalNum = number;
+
+	while (originalNum != 0) {
+		// here remainder contains the last digit
+		remainder = originalNum % 10;
+		result += remainder * remainder * remainder;
+
+		// it will removing last digit from the orignal number
+		originalNum /= 10;
+	}
+	if (result == number) {
+		cout << number << " is an Armstrong number.";
+	}
+
+	else {
+		cout << number << " is not an Armstrong number.";
+	}
+
 }
