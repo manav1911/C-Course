fcebc2e7bcea817e9cc213c457d4ec5383558476
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-06-20 05:22:49 +0300

    Function over loading with different number of arguments

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -3,8 +3,26 @@
 #include<string>
 using namespace std;
 
-int main() {
+void add(int, int);
+void add(int, int, int);
+//void add(string, string);
 
+int main() {
+	add(10, 20);
+	add(1, 2, 3);
+	//add("Manav", "Patel");
 
 	return 0;
 }
+
+void add(int x, int y) {
+	cout << "The sum of the two integer digits is: " << x + y << endl;
+}
+
+void add(int x, int y, int z) {
+	cout << "The sum of the two integer digits is: " << x + y + z << endl;
+}
+
+/*void add(string x, string y) {
+	cout << "The string you added is: " << x + " " + y;
+}*/
