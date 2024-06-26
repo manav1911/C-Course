5fe57fa6955ea1750f4c56cada3ea1d154bf3c67
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-06-26 05:56:26 +0300

    Define structure

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -3,7 +3,30 @@
 #include<string>
 using namespace std;
 
+struct person {    //Definition of structure
+	string name;
+	int age;
+	float weight;
+	float height;
+	double salary;
+};
+
 int main() {
+	person pVar;
+
+	pVar.name = "Goti";
+	pVar.age = 25;
+	pVar.weight = 80.5f;
+	pVar.height = 6.1f;
+	pVar.salary = 100000.95;
+
+	pVar.salary = 90000.34;
+
+	cout << endl << "Name: " << pVar.name;
+	cout << endl << "Salary: " << pVar.salary;
+	cout << endl << "Weight: " << pVar.weight;
+	cout << endl << "Height: " << pVar.height;
+	cout << endl << "Salary: " << pVar.salary;
 
 
 	return 0;
