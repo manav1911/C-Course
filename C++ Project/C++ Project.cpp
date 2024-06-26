273752ef6eaa5902f0005b90a660dd1bad52409b
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-06-26 05:10:05 +0300

    Sum of numbers using recurssion

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -1,10 +1,29 @@
-//
+//Sum of numbers
 #include<iostream>
 #include<string>
 using namespace std;
 
-int main() {
+int sum(int);
 
+int main() {
+	int num = 0;
+	int result = 0;
+
+	cout << "Enter a number: ";
+	cin >> num;
+
+	result = sum(num);
+	cout << endl << result;
 
 	return 0;
+}
+
+int sum(int num) {
+	if (num > 0) {
+		cout << endl << num;
+		return (num + sum(num - 1));
+	}
+	else {
+		return 0;
+	}
 }
