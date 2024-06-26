b7e4f84bbf1ee2ca1a8558e383d0af2918ad8983
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-06-26 06:03:56 +0300

    Example of a structure

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -3,8 +3,31 @@
 #include<string>
 using namespace std;
 
-int main() {
+struct phone {
+	string name;
+	int RAM;
+	int ROM;
+	string processor;
+	int camera;
+	double price;
+};
 
+int main() {
+	phone moto;
+
+	moto.name = "SamMoto";
+	moto.processor = "Snapdragon";
+	moto.price = 4500;
+	moto.camera = 15;
+	moto.RAM = 16;
+	moto.ROM = 132;
+
+	cout << "Name: " << moto.name << endl;
+	cout << "Processor: " << moto.processor << endl;
+	cout << "Price: " << moto.price << endl;
+	cout << "Camera: " << moto.camera << endl;
+	cout << "RAM: " << moto.RAM << endl;
+	cout << "ROM: " << moto.ROM << endl;
 
 	return 0;
 }
