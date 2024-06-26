0a1e7ecf6bb9c7e27ce4a29bfe56200b54832337
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-06-26 05:00:41 +0300

    Recurssion

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -3,8 +3,25 @@
 #include<string>
 using namespace std;
 
-int main() {
+int recursion(int);
 
+
+int main() {
+	int num = 0;
+	int result = 0;
+
+	cout << "Enter a number: ";
+	cin >> num;
+
+	result = recursion(num);
+
+	cout << result;
 
 	return 0;
+}
+
+int recursion(int num) {
+	if (num > 0) {
+		return(num * recursion(num - 1));
+	}
 }
