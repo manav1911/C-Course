cb282334340a82ab6bbc0c95e654d15351bdab73
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-06-27 19:35:09 +0300

    Nesting of structures

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -3,8 +3,38 @@
 #include<string>
 using namespace std;
 
-int main() {
+struct camera {
+	int front;
+	int rear;
+};
 
+struct phone {
+	string name;
+	int RAM;
+	int ROM;
+	string processor;
+	camera camera;
+	double price;
+};
+
+int main() {
+	phone moto;
+
+	moto.name = "SamMoto";
+	moto.processor = "Snapdragon";
+	moto.price = 4500;
+	moto.camera.front = 15;
+	moto.camera.rear = 32;
+	moto.RAM = 16;
+	moto.ROM = 132;
+
+	cout << "Name: " << moto.name << endl;
+	cout << "Processor: " << moto.processor << endl;
+	cout << "Price: " << moto.price << endl;
+	cout << "Front Camera: " << moto.camera.front << endl;
+	cout << "Rear Camera: " << moto.camera.rear << endl;
+	cout << "RAM: " << moto.RAM << endl;
+	cout << "ROM: " << moto.ROM << endl;
 
 	return 0;
 }
