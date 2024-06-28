685e44d2e5c131fcd3dc76fc43b81c729628737b
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-06-28 05:01:23 +0300

    Array of structures

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -3,8 +3,36 @@
 #include<string>
 using namespace std;
 
-int main() {
+struct student {
+	int Adm_no;
+	int percentage;
+	string name;
+};
 
+int main() {
+	struct student stud[3];
+	int i;
+
+	//Taking values from the user and storing the data
+	for (i = 0; i < 3; i++) {
+		cout << "Student: " << i + 1 << endl;
+		
+		cout << "Enter Admission Number: ";
+		cin >> stud[i].Adm_no;
+		cout << "Enter Name: ";
+		cin >> stud[i].name;
+		cout << "Enter percentage: ";
+		cin >> stud[i].percentage;
+	}
+
+	//Print the data
+	cout << "The results are as follows: " << endl;
+	for (i = 0; i < 3; i++) {
+		cout << "Student: " << i + 1 << endl;
+		cout << "Adminssion Number: " << stud[i].Adm_no << endl;
+		cout << "Student Name: " << stud[i].name << endl;
+		cout << "Percentage: " << stud[i].percentage << endl;
+	}
 
 	return 0;
 }
