4e1def6f3f909a3e2318f7daf0922b5475920df5
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-06-28 05:44:15 +0300

    Implicit Conversion

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -7,7 +7,7 @@ int main() {
 	int var1;
 	float var2 = 10.5;
 
-	var1 = var2;
+	var1 = var2 + 45.99 + 'C';
 
 	cout << "The value 1 is: " << var1 << endl;
 	cout << "The value 2 is: " << var2 << endl;
