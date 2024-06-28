caf5f6d94caa3dedd56a31639f46898d6aadc488
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-06-28 05:25:30 +0300

    Create a structure for student details ( Full name, rollno, marks ) and create structure variable & print sample data of student.

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -3,8 +3,34 @@
 #include<string>
 using namespace std;
 
+struct student {
+	string fullName;
+	int admNo;
+	int marks;
+};
+
 int main() {
+	struct student std[1];
+	int i;
 
+	for (i = 0; i < 1; i++) {
+		cout << "Student: " << i + 1 << endl;
+		
+		cout << endl << "Student Full Name: ";
+		getline(cin, std[i].fullName);
+		cout << "Enter Admission Number: ";
+		cin >> std[i].admNo;
+		cout << "Enter Marks: ";
+		cin >> std[i].marks;
+	}
 
+	cout << endl << "The results are below: " << endl;
+	for (i = 0; i < 1; i++) {
+		cout << " " << endl;
+		cout << "Student: " << i + 1 << endl;
+		cout << "Full Name: " << std[i].fullName << endl;
+		cout << "Admission Number: " << std[i].admNo << endl;
+		cout << "Marks: " << std[i].marks << endl;
+	}
 	return 0;
 }
