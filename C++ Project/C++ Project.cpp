1038e4195ee8a1a6508e03132a7183744cfc4c1a
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-06-30 13:43:00 +0300

    Example of Classes and objects

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -3,8 +3,32 @@
 #include<string>
 using namespace std;
 
-int main() {
+class house {
 
+	//Access specifier - Can not be called and edited outside the class
+private:
+
+	//member variables
+	int lenght = 0, breadth =0;
+
+	//Access Specifier - Can be called and edited outside the class
+public:
+	//member functions
+	void setData(int x, int y) {
+		lenght = x;
+		breadth = y;
+	}
+
+	void area() {
+		cout << "The area of the house is: " << lenght * breadth;
+	}
+};
+
+int main() {
+	house h1;
+
+	h1.setData(300, 500);
+	h1.area();
 
 	return 0;
 }
