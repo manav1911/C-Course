1b9e5d31735756dfa99669d0cf12de13b3e32618
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-06-30 13:33:13 +0300

    Expolicit Conversion

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -4,7 +4,12 @@
 using namespace std;
 
 int main() {
+	int var1;
+	float var2 = 10.5;
 
+	var1 = static_cast<int>(var2) + 43.123 + 'C';
+	cout << "\n The value 1 is: " << var1 << endl;
+	cout << "\n The value 2 is: " << var2 << endl;
 
 	return 0;
 }
