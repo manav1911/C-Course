394ebe731a309625c6d71ff3d1b600c9933903a6
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-07-01 05:35:00 +0300

    Copy Constructor

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -3,8 +3,43 @@
 #include<string>
 using namespace std;
 
+class Person {
+private:
+	string name;
+	int age;
+	float height;
+
+public:
+	/*Person() {
+		name = "Null";
+		age = 0;
+		height = 0.0f;
+	}*/
+
+	Person(string name_a, int age_a, float height_a) {
+		name = name_a;
+		age = age_a;
+		height = height_a;
+	}
+
+	Person(Person& obj2) {
+		name = obj2.name;
+		age = obj2.age;
+		height = obj2.height;
+	}
+
+	void getData() {
+		cout << endl << "Name: " << name;
+		cout << endl << "Age: " << age;
+		cout << endl << "Height: " << height;
+	}
+};
+
 int main() {
-
-
+	/*Person obj;*/
+	Person obj2("MP", 24, 5.10f), obj3(obj2);
+	/*obj.getData();*/
+	obj2.getData();
+	obj3.getData();
 	return 0;
 }
