c84b5963f528ecd31c165d173680aa82e47eef3d
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-07-02 06:10:10 +0300

    Write a C++ Program to implement Pre And Post Decrement operator.

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -3,8 +3,37 @@
 #include<string>
 using namespace std;
 
-int main() {
+class Person {
+private:
+	int weight;
 
+public:
+	Person(int x = 0) {
+		weight = x;
+	}
+
+	void getData() {
+		cout << "Weight is: " << weight << endl;
+	}
+
+	void operator--() {
+		--weight;
+	}
+
+	void operator--(int) {
+		weight--;
+	}
+};
+
+int main() {
+	Person Jon(78);
+	--Jon;
+
+	Person Chris(52);
+	Chris--;
+
+	Jon.getData();
+	Chris.getData();
 
 	return 0;
 }
