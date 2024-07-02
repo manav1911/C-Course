d9dab47c98b7cc31a79cd4e33c7509a066cd5677
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-07-02 05:56:07 +0300

    Operator overloading example

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -3,8 +3,39 @@
 #include<string>
 using namespace std;
 
-int main() {
+class A {
+private:
+	int weight;
 
+public:
+	A(int x = 0) {
+		weight = x;
+	}
+
+	void getData() {
+		cout << "Weight is: " << weight << endl;
+	}
+
+	A operator + (A w) {
+		A temp;
+		temp.weight = weight + w.weight;
+		return temp;
+	}
+};
+
+int main() {
+	int a = 34, b = 45, c = 0;
+	c = a + b;
+
+	cout << endl << "Total: " << c << endl;
+
+	//Operator Overloading
+	A person1(89);
+	A person2(99);
+	A total;
+
+	total = person1 + person2;
+	total.getData();
 
 	return 0;
 }
