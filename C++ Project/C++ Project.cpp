b3cd51ffdd927dde84b14f41ceb37fcdfd22276e
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-07-02 06:04:50 +0300

    Pre Increment and post increment ovverloading

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -3,8 +3,35 @@
 #include<string>
 using namespace std;
 
-int main() {
+class Person {
+private:
+	int weight;
 
+public:
+	Person(int x = 0) {
+		weight = x;
+	}
+
+	void getData() {
+		cout << "Weight is: " << weight << endl;
+	}
+
+	void operator++() {
+		++weight;
+	}
+
+	void operator++(int) {
+		weight++;
+	}
+};
+
+int main() {
+	Person Jon(78);
+	++Jon;
+
+	Jon++;
+
+	Jon.getData();
 
 	return 0;
 }
