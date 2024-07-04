6e2d30708f367abfde904694e904f435561e3d81
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-07-04 05:52:38 +0300

    Inheritance Example 1

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -3,8 +3,36 @@
 #include<string>
 using namespace std;
 
-int main() {
+class Rectangle {
+public:
+	int length;
+	int width;
+	/*int area = length * width;*/
 
+	void totalArea() {
+		cout << "The area of the rectangle is: " << length * width << endl;
+	}
+};
+
+class Cuboid :public Rectangle {
+public:
+	int height;
+	/*int volume = length * width * height;*/
+
+	void totalVolume() {
+		cout << "The volume of the coboid is: " << length * width * height << endl;
+	}
+};
+
+int main() {
+	Cuboid c;
+
+	c.length = 10;
+	c.width = 20;
+	c.height = 30;
+
+	c.totalArea();
+	c.totalVolume();
 
 	return 0;
 }
