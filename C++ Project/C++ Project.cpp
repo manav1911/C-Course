b033c685598f53f672d780f5cff29b473d3c176e
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-07-04 06:01:44 +0300

    Calling the default constructor

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -3,7 +3,22 @@
 #include<string>
 using namespace std;
 
+class base {
+public:
+	base() {
+		cout << "Default constructor of base class." << endl;
+	}
+	base(int b) {
+		cout << "Para constrcutor of base class" << endl;
+	}
+};
+
+class derived : public base {
+	//EMPTY
+};
+
 int main() {
+	derived d;
 
 
 	return 0;
