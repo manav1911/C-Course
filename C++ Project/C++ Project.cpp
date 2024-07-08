885497608b322269f3b315dc6ec8c66a1f6c1c62
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-07-08 05:14:14 +0300

    Parameterized Constructor

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -3,7 +3,29 @@
 #include<string>
 using namespace std;
 
+class base {
+public:
+	base() {
+		cout << "Default constructor of base class." << endl;
+	}
+	base(int b) {
+		cout << "Para constructor of base class" << endl;
+	}
+};
+
+class derived : public base {
+public:
+	derived() {
+		cout << "Default Constructor of Derived Class." << endl;
+	}
+	derived(int d) : base(d) {
+		cout << "Para Constructor of Derived Class." << endl;
+	}
+};
+
 int main() {
+	//derived d;
+	derived d(9);
 
 
 	return 0;
