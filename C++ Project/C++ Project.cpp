3fbb6b5e5102a6182b8fc4a0a4f1662187ce1551
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-07-08 05:29:18 +0300

    Function Overloading

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -3,8 +3,24 @@
 #include<string>
 using namespace std;
 
-int main() {
+class base {
+public:
+	void Msg() {
+		cout << "It is a base class" << endl;
+	}
+};
 
+class derived : public base {
+public:
+	void Msg() {
+		cout << "It is a derived class" << endl;
+		base::Msg();
+	}
+};
+
+int main() {
+	derived d;
+	d.Msg();
 
 	return 0;
 }
