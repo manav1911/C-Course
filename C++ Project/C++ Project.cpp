a60c2bf227fa67e8e30db07842767ae7e8ec425d
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-07-09 05:35:04 +0300

    Introduction to Pointer

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -4,7 +4,12 @@
 using namespace std;
 
 int main() {
+	int number = 10;
+	int* ptr = NULL;
 
+	ptr = &number;
+	cout << "ptr: " << ptr << endl;
+	cout << "Value in ptr: " << *ptr << endl;
 
 	return 0;
 }
