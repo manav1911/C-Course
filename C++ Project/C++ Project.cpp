431a35e9535ea3b7d16f61749338ac25d702e444
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-07-09 05:52:51 +0300

    Pointer and function

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -3,8 +3,21 @@
 #include<string>
 using namespace std;
 
-int main() {
+void Square(int* ptr);
 
+int main() {
+	int number = 10;
+	cout << "Before: " << number << endl;
+
+	Square(&number);
+	cout << "After: " << number << endl;
 
 	return 0;
+}
+
+void Square(int *ptr) {
+	int temp = *ptr;
+	temp = temp * temp;
+
+	*ptr = temp;
 }
