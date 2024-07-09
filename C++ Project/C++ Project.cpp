bddaed2b0e15fb233c91e80d25046f4c4850dc30
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-07-09 05:46:05 +0300

    Pointers and Array

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -6,6 +6,7 @@ using namespace std;
 int main() {
 	int arr[5] = { 10,20,30,40,50 };
 	int* ptr = &arr[0];
+	//int* ptr = arr;
 
 	for (int i = 0; i < 5; i++) {
 		cout << "Value at " << i << " is: " << *(ptr + i) << endl;
