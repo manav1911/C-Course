2a06602b4950fb7b64a34744f3a36240bb4b51da
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-07-11 05:28:58 +0300

    Using a constant

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -2,9 +2,20 @@
 #include<iostream>
 #include<string>
 using namespace std;
+const int MAX = 5;
+
+void printArray(int* ptr);
 
 int main() {
-
+	int arr[MAX] = { 10,20,30,40,50 };
+	
+	printArray(arr);
 
 	return 0;
+}
+
+void printArray(int* ptr) {
+	for (int i = 0; i < MAX; i++) {
+		cout << endl << *ptr++;
+	}
 }
