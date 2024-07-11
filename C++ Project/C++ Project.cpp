e4d38adb9074c1ccd552c5fbbbe56fc4200a8be3
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-07-11 05:55:16 +0300

    new operator in memory management

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -3,8 +3,25 @@
 #include<string>
 using namespace std;
 
-int main() {
+class Test {
+private:
+	int data;
 
+public:
+	void setData(int set) {
+		data = set;
+	}
+	int getData() {
+		return data;
+	}
+};
+
+int main() {
+	Test* ptr = new Test;
+
+	ptr->setData(10);
+
+	cout << "Value is: " << ptr->getData();
 
 	return 0;
 }
