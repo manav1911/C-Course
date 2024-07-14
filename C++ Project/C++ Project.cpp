11617bf32d44a3983e525a2c3bab36f812bcb5d6
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-07-14 19:18:10 +0300

    "this" keyword

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -3,8 +3,27 @@
 #include<string>
 using namespace std;
 
-int main() {
+class Person {
+private:
+	int age;
+	string name;
 
+public:
+	Person(int age, string name) {
+		this->age = age;
+		this->name = name;
+	}
+
+	void printValue() {
+		cout << "Age: " << age << endl;
+		cout << "Name: " << name << endl;
+	}
+};
+
+int main() {
+	Person a(45, "John");
+
+	a.printValue();
 
 	return 0;
 }
