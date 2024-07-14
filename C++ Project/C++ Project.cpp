4d95d12ac6bbf3bd222cd81622678d569e838d89
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-07-14 18:29:08 +0300

    Application to show the top speed and weight of cars using pointers

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -3,8 +3,51 @@
 #include<string>
 using namespace std;
 
+class Car {
+private:
+	int weight;
+
+public:
+	void setWeight(int w) {
+		weight = w;
+	}
+	int getWeight() {
+		return weight;
+	}
+};
+
+class BMW {
+private:
+	int topSpeed;
+	Car* ptr;
+
+public:
+	BMW(int wght = 0, int tSpeed = 0) {
+		ptr = new Car;
+		ptr->setWeight(wght);
+
+		topSpeed = tSpeed;
+	}
+
+	int getWeight() {
+		return ptr->getWeight();
+	}
+
+	int getTopSpeed() {
+		return topSpeed;
+	}
+
+	~BMW() {
+		delete ptr;
+	}
+};
+
 int main() {
 
+	BMW b(3000, 350);
+
+	cout << "The weight of the car is: " << b.getWeight() << endl;
+	cout << "The top speed of the car is: " << b.getTopSpeed() << endl;
 
 	return 0;
 }
