ece722170258c34bc3dc66ee7cf4e515818c7eed
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-07-14 19:57:24 +0300

    Destructors

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -3,8 +3,31 @@
 #include<string>
 using namespace std;
 
+class Test{
+private:
+	int *age;
+
+public:
+	Test() {
+		age = new int();
+		*age = 0;
+		cout << "Default Constructor" << endl;
+	}
+
+	Test(int x) {
+		age = new int();
+		*age = x;
+		cout << "Parametrized Constructor" << endl;
+	}
+
+	~Test() {
+		delete age;
+		cout << "Destructor" << endl;
+	}
+};
+
 int main() {
-
-
+	Test obj1;
+	Test obj2(10);
 	return 0;
 }
