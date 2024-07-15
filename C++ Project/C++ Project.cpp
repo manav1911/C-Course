916c159377b3508e0d3b8f652523cfbd2508ca1f
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-07-15 05:47:42 +0300

    Abstract Class

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -3,8 +3,35 @@
 #include<string>
 using namespace std;
 
-int main() {
+class Parent {    //Abstract Class
+public:
+	virtual void show() {
+		cout << "Bun" << endl;
+	}
+};
 
+class boy : public Parent {
+public:
+	void show() {
+		cout << "Blue Bun" << endl;
+	}
+};
+
+class girl : public Parent {
+public:
+	void show() {
+		cout << "Pink Bun" << endl;
+	}
+};
+
+int main() {
+	Parent* ptr;
+
+	ptr = new boy();
+	ptr->show();
+
+	ptr = new girl();
+	ptr->show();
 
 	return 0;
 }
