454bb88c3665dd4cb4d68ba9f634b4c946089496
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-07-15 05:30:44 +0300

    Virtual Class

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -3,8 +3,34 @@
 #include<string>
 using namespace std;
 
-int main() {
+class base {
+public:
+	virtual void show(){
+		cout << "Base Class" << endl;
+	}
+};
 
+class derived1 : public base {
+public:
+	void show() {
+		cout << "Derived 1" << endl;
+	}
+};
+
+class derived2 : public base {
+public:
+	void show() {
+		cout << "Derived 2" << endl;
+	}
+};
+
+int main() {
+	base* ptr;
+	ptr = new derived1;
+	ptr->show();
+
+	ptr = new derived2;
+	ptr->show();
 
 	return 0;
 }
