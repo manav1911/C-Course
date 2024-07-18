652559112f92f4e15a89a6b4e9650ea6bb3e737c
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-07-18 05:50:26 +0300

    Operator Overloading with friend function

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -3,8 +3,52 @@
 #include<string>
 using namespace std;
 
-int main() {
+class Person {
+private:
+	int weight;
 
+public:
+	Person(int w = 0) {
+		weight = w;
+	}
+
+	friend bool operator>(Person x, Person y);
+	friend bool operator<(Person x, Person y);
+	friend bool operator==(Person x, Person y);
+};
+
+bool operator<(Person x, Person y) {
+	return x.weight < y.weight;
+}
+
+bool operator>(Person x, Person y) {
+	return x.weight > y.weight;
+}
+
+bool operator==(Person x, Person y) {
+	return x.weight == y.weight;
+}
+
+int main() {
+	int w1, w2;
+	
+	cout << "Enter Jon's weight: " << endl;
+	cin >> w1;
+	cout << "Enter Rick's weight: " << endl;
+	cin >> w2;
+
+	Person jon(w1);
+	Person rick(w2);
+
+	if (jon > rick) {
+		cout << "Jon is heavier" << endl;
+	}
+	else if (jon < rick) {
+		cout << "Rick is heavier" << endl;
+	}
+	else {
+		cout << "Jon is equally heavy as Rick" << endl;
+	}
 
 	return 0;
 }
