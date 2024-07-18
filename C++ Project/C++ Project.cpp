e892a1a06b390ea487d7dc6658474e99658c4523
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-07-18 05:56:08 +0300

    Static Memebr Variable

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -3,8 +3,30 @@
 #include<string>
 using namespace std;
 
-int main() {
+class Alpha {
+private:
+	int a;
+	int b;
 
+public:
+	Alpha() {
+		a = 5;
+		b - 5;
+		stat++;
+	}
+
+	static int stat;
+};
+int Alpha::stat;
+
+int main() {
+	Alpha a1;
+	Alpha a2;
+	Alpha a3;
+
+	cout << "Value using a1 obj = " << a1.stat << endl;
+	cout << "Value using a2 obj = " << a2.stat << endl;
+	cout << "Value using class name = " << Alpha::stat << endl;
 
 	return 0;
 }
