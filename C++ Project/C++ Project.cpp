6ba102df94395b0f230873739075ce7662ba691f
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-07-18 05:23:16 +0300

    Friend Class

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -3,8 +3,38 @@
 #include<string>
 using namespace std;
 
-int main() {
+class Alpha {
+private:
+	int a1;
 
+public:
+	Alpha(int arg = 0) {
+		a1 = arg;
+	}
+
+	friend class Beta;
+};
+
+class Beta {
+private:
+	int b1;
+
+public:
+	Beta(int arg = 0) {
+		b1 = arg;
+	}
+
+	int sum() {
+		Alpha a(9);
+
+		int sum = a.a1 + b1;
+		return sum;
+	}
+};
+
+int main() {
+	Beta b(2);
+	cout << "The sum is: " << b.sum() << endl;
 
 	return 0;
 }
