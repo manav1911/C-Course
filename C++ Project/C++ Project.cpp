765be2b9093fd051ea98fad53c5f821b88801f99
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-07-23 05:38:06 +0300

    Reading the file using fstream

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -1,10 +1,22 @@
 //
 #include<iostream>
 #include<string>
+#include<fstream>
 using namespace std;
 
 int main() {
+	ifstream fin;   //create input stream
+	char ch;  //Variable for storing GET pointer character
 
+	fin.open("my.txt");    //Load file in RAM
+	fin >> ch;   //Storing 1st GET pointer value into CH variable
+
+	while (!fin.eof()) {  //Iterate till END OF FILE
+		cout << ch;   //Print the value at first index (1st character)
+		fin >> ch;    //move the GET pointer forward
+	}
+
+	fin.close();  //Close file
 
 	return 0;
 }
