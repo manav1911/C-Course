a74a9607fd490ea321a24269c7bb207c1130d4c3
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-07-23 05:20:32 +0300

    Writing data to file using fstream

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -1,10 +1,17 @@
 //
 #include<iostream>
 #include<string>
+#include<fstream>
 using namespace std;
 
 int main() {
+	ofstream fout;
 
+	fout.open("my.txt");
+
+	fout << "C++Course";
+
+	fout.close();
 
 	return 0;
 }
