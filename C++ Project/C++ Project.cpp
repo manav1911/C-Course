640c7d923f3c7be08e86de72c4d8726aa6fce585
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-07-23 05:46:03 +0300

    Tellg Function to display the position

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -1,10 +1,24 @@
 //
 #include<iostream>
 #include<string>
+#include<fstream>
 using namespace std;
 
 int main() {
+	ifstream fin;
+	char ch;
 
+	fin.open("my.txt");
+	int pos;
+	pos = fin.tellg();
+
+	cout << pos << endl;
+	fin >> ch;
+
+	pos = fin.tellg();
+	cout << pos << endl;
+
+	fin.close();
 
 	return 0;
 }
