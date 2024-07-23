4f0fd3cbc88134cb4aa7516c0a4ae3d68f1b6fe8
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-07-23 04:47:46 +0300

    Static Member Function

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -3,8 +3,31 @@
 #include<string>
 using namespace std;
 
-int main() {
+class Alpha {
+private:
+	int a;
+	int b;
 
+public:
+	Alpha() {
+		a = 5;
+		b - 5;
+	}
+
+	static int stat;
+
+	static int getStat() {
+		stat++;
+		return stat;
+	}
+};
+int Alpha::stat = 0;
+
+int main() {
+	Alpha a1;
+
+	cout << "Value using a1 obj = " << a1.getStat() << endl;
+	cout << "Value using class name = " << Alpha::getStat() << endl;
 
 	return 0;
 }
