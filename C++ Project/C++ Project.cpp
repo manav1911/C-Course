f8b16c87e36534961eced37a69082ff417faa7a0
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-07-26 13:26:19 +0300

    TellG Function

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -1,10 +1,25 @@
 //
 #include<iostream>
 #include<string>
+#include<fstream>
 using namespace std;
 
 int main() {
+	ofstream fout;
 
+	fout.open("my.txt", ios::app);
+
+	int pos;
+	pos = fout.tellp();
+
+	cout << "Put pointer is at: " << pos << endl;
+	
+	fout << "Academy";
+
+	pos = fout.tellp();
+	cout << "Put pointer after text: " << pos;
+
+	fout.close();
 
 	return 0;
 }
