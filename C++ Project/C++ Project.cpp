41234e7d7305839677b9a00fb0eb086825e8be1a
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-07-26 18:47:04 +0300

    Seekp Function

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -1,10 +1,21 @@
 //
 #include<iostream>
 #include<string>
+#include<fstream>
 using namespace std;
 
 int main() {
+	ofstream fout;
+	char A[8] = "Academy";
 
+	fout.open("my.txt", ios::in);
+
+	fout.seekp(3, ios_base::beg);
+
+	fout.write(A, 8);
+
+	fout.close();
+	return 0;
 
 	return 0;
 }
