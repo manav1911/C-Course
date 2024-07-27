b4e28b517a46b689a956364fde254f65c5686d50
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-07-27 13:08:59 +0300

    Exception Handling String Value

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -4,7 +4,27 @@
 using namespace std;
 
 int main() {
+	int num1 = 0, num2 = 0, total = 0;
 
+	cout << "Enter the two numbers: ";
+	cin >> num1 >> num2;
+
+	try {
+		if (num2 == 0) {
+			string err = "Number divided by 0";
+			throw err;
+		}
+		else {
+			total = num1 / num2;
+			cout << "The output is: " << total << endl;
+		}
+	}
+	catch (int x) {
+		cout << "Exception is: Divide by " << x;
+	}
+	catch (string y) {
+		cout << y;
+	}
 
 	return 0;
 }
