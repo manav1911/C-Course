4b2786acef8a58a80babbc84dbb8abd6e056c0cb
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-07-27 13:05:40 +0300

    Exception Handling with Integers

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -4,7 +4,23 @@
 using namespace std;
 
 int main() {
+	int num1 = 0, num2 = 0, total = 0;
 
+	cout << "Enter the two numbers: ";
+	cin >> num1 >> num2;
+
+	try {
+		if (num2 == 0) {
+			throw num2;
+		}
+		else {
+			total = num1 / num2;
+			cout << "The output is: " << total << endl;
+		}
+	}
+	catch (int x) {
+		cout << "Exception is: Divide by " << x;
+	}
 
 	return 0;
 }
