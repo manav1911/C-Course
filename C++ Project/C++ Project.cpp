ae6aa6857b1e6d42c975cdbfad7347c6410bbc53
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-08-07 06:05:20 +0300

    Queue Container

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -1,10 +1,30 @@
 //
 #include<iostream>
 #include<string>
+#include<queue>
 using namespace std;
 
 int main() {
+	//Declaration
+	queue<int> queue1;
 
+	//Push values
+	queue1.push(10);
+	queue1.push(20);
+	queue1.push(30);
+
+	//Front
+	cout << "Front Element: " << queue1.front() << endl;
+
+	//Back
+	cout << "Front Element: " << queue1.back() << endl;
+
+	//Pop
+	queue1.pop();
+	cout << "New front element: " << queue1.front() << endl;
+
+	//Size: Number of elements present in the Queue
+	cout << "Number of elements: " << queue1.size();
 
 	return 0;
 }
