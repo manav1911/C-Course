5679bc722cb82cf4aed6ba9cfa624a51b242a3dd
ManavKumar RakeshKumar Patel <95034471+manav1911@users.noreply.github.com>
2024-08-07 05:54:04 +0300

    Stack containers

--- C++ Project/C++ Project.cpp
+++ C++ Project/C++ Project.cpp
@@ -1,10 +1,27 @@
 //
 #include<iostream>
 #include<string>
+#include<stack>
 using namespace std;
 
 int main() {
+	//Declaration
+	stack<int> stack1;
 
+	//Push Value
+	stack1.push(10);
+	stack1.push(20);
+	stack1.push(30);
+
+	//Top
+	cout << "Top element: " << stack1.top() <<endl;
+
+	//Pop
+	stack1.pop();
+	cout << "New Top: " << stack1.top() << endl;
+
+	//Size: Total number of elements present in stack
+	cout << "Number of element: " << stack1.size();
 
 	return 0;
 }
